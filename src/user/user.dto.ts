26aad220cfbbfb90cb384a5d873ebbd21ee55467
ismaelforj <ismael@forj.network>
2023-02-25 02:24:25 -0300

    added email validation.

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class UserDto {
   @ApiProperty({
     description: 'This will identify the unique user name in our database.',
   })
   @IsString()
+  @IsEmail()
   email: string;
 
   @ApiProperty({
