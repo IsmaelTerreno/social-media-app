38984c6587398aee8595d5a2cbf06cecbe3d63f2
ismaelforj <ismael@forj.network>
2023-02-24 21:38:24 -0300

    Adding the base documentation with Swagger support.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Social media application')
+    .setDescription('API to create tweets for any topic you want.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
